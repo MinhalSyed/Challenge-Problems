5e1d553745ac2ad786b57562bd65bcc9e80b3aeb
Minhal Syed <minhal.syed2@gmail.com>
2021-01-15 23:03:47 -0500

    Maximum SubArray

---
+++ MaximumSubarray.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int N = nums.Count();
+        int[,] Sum = new int[N, N];
+        
+        for(int i = 0; i < N; i++){
+            for(int j = i; j < N; j++){
+                if(i == j){
+                    Sum[i,j] = nums[i];
+                }
+                else
+                {
+                    Sum[i,j] = nums[j] + Sum[i,j-1];
+                }
+                
+                if(Sum[i,j] > maxSum){
+                    maxSum = Sum[i,j];
+                }
+            }
+        }
+        
+        return maxSum;
+        
+    }
+}
