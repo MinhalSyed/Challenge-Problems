85962fbcade6fc61775c729319696b93faf09892
Minhal Syed <minhal.syed2@gmail.com>
2021-02-13 16:42:44 -0500

    fixed

--- Easy/MergeSortedArray.cs
+++ Easy/MergeSortedArray.cs
@@ -1,69 +1,43 @@
-/*
- I'm stupid.
- Just start at the end of nums1. :facepalm:
-*/
 public class Solution {
     public void Merge(int[] nums1, int m, int[] nums2, int n) {
         
-        Rotate(nums1, n);
+        int i = m - 1;
         
-        int i = n;
-        int j = 0;
+        int j = n - 1;
         
-        int ii = 0;
+        int ii = m + n - 1;
         
-        while (ii < m+n && j < n && i < m + n)
+        while(i >= 0 && j >= 0)
         {
-            if(nums1[i] <= nums2[j])
+            if(nums1[i] >= nums2[j])
             {
                 nums1[ii] = nums1[i];
-                i++;
+                i--;
             }
             else
             {
                 nums1[ii] = nums2[j];
-                j++;
+                j--;
             }
             
-            ii++;
+            ii--;
         }
         
-        while(ii < m+n){
+        while(ii >=0 ){
             
-            if(i >= m+n)
+            if(i < 0)
             {
                 nums1[ii] = nums2[j];
-                
-                j++;
-            }
-            else if (j >= n)
-            {
-                nums1[ii] = nums1[i];
-                i++;
+                j--;
             }
             
-            ii++;
-        }
-        
-    }
-    
-    public void Rotate(int[] nums, int k) {
-        k = k % nums.Count();
-        
-        ReverseArray(0, nums.Count()-1, nums);
-        ReverseArray(0, k-1, nums);
-        ReverseArray(k,nums.Count()-1 , nums);
-    }
-    
-    private void ReverseArray(int startIndex, int endIndex, int[] nums)
-    {
-        while(startIndex < endIndex)
-        {
-            int temp = nums[startIndex];
-            nums[startIndex] = nums[endIndex];
-            nums[endIndex] = temp;
-            startIndex++;
-            endIndex--;
+            else if (j < 0)
+            {
+                nums1[ii] = nums1[i];
+                i--;
+            }
+            
+            ii--;
         }
     }
 }
