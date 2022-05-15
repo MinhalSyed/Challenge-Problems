3b3c52d87d99c2a1a8490e916df3deda8a7c8352
MinhalSyed <minhal.syed2@gmail.com>
2022-05-15 13:03:12 -0400

    Update K-Closest-Points-to-Origin.py

--- Medium/K-Closest-Points-to-Origin.py
+++ Medium/K-Closest-Points-to-Origin.py
@@ -3,17 +3,16 @@ from queue import PriorityQueue
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        distance_squared = [x**2 + y**2 for x, y in points]
-               
+              
         queue = PriorityQueue()
         
-        for index, distance in enumerate(distance_squared):
-            queue.put((distance, index))
+        for index, (x,y) in enumerate(points):
+            queue.put((x**2 + y**2, index))
         
         closest_k_points = []
         
         while not queue.empty() and len(closest_k_points) < k:
-            distance_point, closest_point_index = queue.get()
+            closest_point_distance, closest_point_index = queue.get()
             closest_k_points.append(points[closest_point_index])
         
         return closest_k_points
